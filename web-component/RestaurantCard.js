d330ed9d9979de72ebd3361a6ddea2dedef5e10d
Tibor Fazekas <tibcso0322@gmail.com>
2024-02-27 23:41:46 +0100

    kind of finished

--- web-component/RestaurantCard.js
+++ web-component/RestaurantCard.js
@@ -11,26 +11,34 @@ class RestaurantCard extends HTMLElement {
 	connectedCallback() {
 		const vertical = () => {
 			this.innerHTML = '';
-
+			
 			const div = document.createElement('div')
 			const img = document.createElement('img')
 			const button = document.createElement('button')
 			const h1 = document.createElement('h1')
 			const p = document.createElement('p')
-	
+			
+			div.style.display = 'flex'
+			div.style.flexDirection = 'column'
+			div.style.alignItems = 'start'
+
 			h1.innerHTML = this.getAttribute('title')
 			h1.style.fontWeight = '600'
 			h1.style.fontSize = '2rem';
-
+			
 			img.src = this.getAttribute('imageSrc')
 			img.style.width = '90%'
-			img.style.height = '13rem'
+			img.style.height = '14rem'
 			img.style.borderRadius = '.5rem';
-	
+			img.style.margin = 'auto'
+			
 			const id = this.getAttribute('id')
 			button.onclick = () => this.dispatchEvent(new CustomEvent('select', {  bubbles: true, detail: { id }}))
-			button.textContent = 'Continue'
-	
+			button.innerHTML = 'Continue &raquo;'
+			button.style.marginLeft = 'auto'
+			button.style.color = '#803cec'
+			button.style.fontWeight = '700'
+			
 			p.textContent = this.initialText;
 	
 			div.appendChild(img)
@@ -42,8 +50,47 @@ class RestaurantCard extends HTMLElement {
 
 		const horizontal = () => {
 			this.innerHTML = '';
+			
+			const div = document.createElement('div')
+			const div2 = document.createElement('div')
+			const img = document.createElement('img')
+			const button = document.createElement('button')
+			const h1 = document.createElement('h1')
+			const p = document.createElement('p')
+			
+			div.style.display = 'flex'
+			div2.style.display = 'flex'
+			div.style.justifyContent = 'space-between'
+			div2.style.justifyContent = 'space-between'
 
-
+			h1.innerHTML = this.getAttribute('title')
+			h1.style.fontWeight = '600'
+			h1.style.fontSize = '2rem';
+			
+			img.src = this.getAttribute('imageSrc')
+			img.style.width = '70%'
+			img.style.height = '10rem'
+			img.style.borderRadius = '.5rem';
+			
+			const id = this.getAttribute('id')
+			button.onclick = () => this.dispatchEvent(new CustomEvent('select', {  bubbles: true, detail: { id }}))
+			button.innerHTML = 'Continue &raquo;'
+			button.style.marginLeft = 'auto'
+			button.style.color = '#803cec'
+			button.style.fontWeight = '700'
+			
+			p.textContent = this.initialText;
+			p.style.color = 'rgba(0, 0, 0, 0.5)'
+	
+			div.appendChild(h1)
+			div.appendChild(button)
+			div2.appendChild(img)
+			div2.appendChild(p)
+			this.appendChild(div)
+			this.appendChild(div2)
+			this.style.display = 'flex'
+			this.style.flexDirection = 'column'
+			this.style.gap = '.6rem'
 		}
 
 		const observer = new MutationObserver((mutationsList, observer) => {
@@ -62,7 +109,7 @@ class RestaurantCard extends HTMLElement {
 		const config = { attributes: true };
 		observer.observe(this, config);
 
-		vertical();
+		this.getAttribute('layout') === 'vertical' ? vertical(): horizontal()
 	}
 }
 
