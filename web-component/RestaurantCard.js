ddbbfecdee8b2dc0d491fef8c5b1644cfce8bd4f
Tibor Fazekas <tibcso0322@gmail.com>
2024-02-27 23:17:36 +0100

    almost done

--- web-component/RestaurantCard.js
+++ web-component/RestaurantCard.js
@@ -8,34 +8,39 @@ class RestaurantCard extends HTMLElement {
 	}
 
 	connectedCallback() {
+		const div = document.createElement('div')
+		const img = document.createElement('img')
 		const button = document.createElement('button')
+		const h1 = document.createElement('h1')
+		const p = document.createElement('p')
+
+		h1.innerHTML = this.getAttribute('title')
+		img.src = this.getAttribute('imageSrc')
+
 		const id = this.getAttribute('id')
-		button.onclick = () => this.dispatchEvent(new Event('select', {  bubbles: true, detail: { id: 1 }}))
-		button.textContent = 'fesf'
-		const vertical = `<div>
-			<img src=${this.getAttribute('imageSrc')} />
-			<h1>${this.getAttribute('title')}<h1/>
-		<div/>`;
-		const horizontal = `<div>
-			horizontal
-		<div/>`;
+		button.onclick = () => this.dispatchEvent(new CustomEvent('select', {  bubbles: true, detail: { id }}))
+		button.textContent = 'Continue'
+
+		p.textContent = this.textContent.trim()
+
+		div.appendChild(img)
+		div.appendChild(h1)
+		div.appendChild(p)
+		div.appendChild(button)
+
 		const observer = new MutationObserver((mutationsList, observer) => {
 			// Iterate over each mutation
 			for(const mutation of mutationsList) {
 				if (mutation.type === 'attributes') {
 				// React to the attribute change
 				console.log(`Attribute ${mutation.attributeName} changed to: ${this.getAttribute(mutation.attributeName)}`);
-				this.innerHTML = this.getAttribute('layout') === 'vertical' ? vertical: horizontal;
 			}
 		}})
 
 		const config = { attributes: true };
 		observer.observe(this, config);
-
-		this.innerHTML = this.getAttribute('layout') === 'vertical' ? vertical: horizontal;
-		this.appendChild(button)
-		
-
+		this.innerHTML = '';
+		this.appendChild(div)
 	}
 }
 
